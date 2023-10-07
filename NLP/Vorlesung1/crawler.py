151b9f11272f38bac5bc763d33ce46513d0c54bc
Fabian Schmidt <106911320+Schmiddi3110@users.noreply.github.com>
2023-10-07 09:42:36 +0200

    NLP_Lecture1
    Topics: Crawling of websites and PDFs

---
+++ NLP/Vorlesung1/crawler.py
@@ -0,0 +1,19 @@
+import re 
+import json 
+import requests 
+
+from bs4 import BeautifulSoup
+from urllib.parse import urlparse
+
+def crawl(url):
+    # TODO
+    return
+
+def crawl_with_links(url, link_pattern, n_links=20):
+    # TODO
+    return
+
+if __name__ == "__main__":
+    # TODO
+    # use https://en.wikipedia.org/wiki/Hannibal
+    
---
+++ NLP/Vorlesung1/pdfparser.py
@@ -0,0 +1,15 @@
+from PyPDF4 import PdfFileReader, PdfFileWriter
+
+def extract_pages(pdf_path):
+    # TODO
+    return
+
+from pdfminer.high_level import extract_pages
+from pdfminer.layout import LTTextContainer
+
+def extract_pages_miner(pdf_path):
+    # TODO
+    return
+
+if __name__ == "__main__":
+    # TODO
