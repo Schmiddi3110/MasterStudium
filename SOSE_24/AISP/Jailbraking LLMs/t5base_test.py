84bd41f964b3bd9f40609f32ec448b76a68141de
Fabian Schmidt <106911320+Schmiddi3110@users.noreply.github.com>
2024-05-02 13:06:26 +0200

    stuff

--- SOSE_24/AISP/Jailbraking LLMs/t5base_test.py
+++ SOSE_24/AISP/Jailbraking LLMs/t5base_test.py
@@ -1,7 +1,7 @@
 from transformers import T5Tokenizer, T5ForConditionalGeneration
 
-tokenizer = T5Tokenizer.from_pretrained("google-t5/t5-small")
-model = T5ForConditionalGeneration.from_pretrained("google-t5/t5-small")
+tokenizer = T5Tokenizer.from_pretrained("google-t5/t5-base")
+model = T5ForConditionalGeneration.from_pretrained("google-t5/t5-base")
 
 input_ids = tokenizer("translate English to German: AI Security and Privacy is a nice lecture.", return_tensors="pt").input_ids
 outputs = model.generate(input_ids)
