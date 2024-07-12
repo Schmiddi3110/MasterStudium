4de7f6a874640e8fdf9afb71efc34618397a1163
Fabian Schmidt <fepschmidt@gmail.com>
2024-07-12 15:06:12 +0200

    aisp stuff

--- SOSE_24/AISP/Projektarbeit/project_poison_model.py
+++ SOSE_24/AISP/Projektarbeit/project_poison_model.py
@@ -1,4 +1,3 @@
-# %% [markdown]
 # # Data Poisoning
 # 
 # This code is originally based on an exmaple notebook from ART:
@@ -10,74 +9,27 @@
 
 
 
-# %%
+
 import numpy as np
 import os, sys
 import tqdm
 from tqdm import trange
-
-# %%
-
-module_path = os.path.abspath(os.path.join('..'))
-if module_path not in sys.path:
-    sys.path.append(module_path)
-module_path = os.path.abspath(os.path.join('.'))
-if module_path not in sys.path:
-    sys.path.append(module_path)
-
-
-# %%
-
 from art.utils import load_cifar10
-
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.utils.data import TensorDataset, DataLoader
-
-
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu") # mps does not work
-
-
-# Model 
-
-
 import timm
 
 def _create_model(model_name, classes):
-    model = timm.create_model(MODEL_NAME, pretrained=True) # TODO Consider pretrained=True and False
+    model = timm.create_model(model_name, pretrained=True) # TODO Consider pretrained=True and False
     in_feats = model.fc.in_features
     model.fc = nn.Linear(in_feats, classes) # adapt final layer to match the 10 classes of CIFAR-10
 
     return model
 
-MODEL_NAME = "resnet18.a1_in1k"
-model = _create_model(MODEL_NAME, 10)
-upsampler = torch.nn.Upsample(scale_factor=7, mode='nearest')   # 32 --> 224, Faktor 7
-optimizer = torch.optim.SGD(model.parameters(), lr=0.1, momentum=0.9, weight_decay=5e-4, nesterov=True) # from poisoning example
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu") # mps does not work
 
-loss_fn = torch.nn.CrossEntropyLoss()
-
-
-
-# %%
-# Data with upsampling
-(x_train, y_train), (x_test, y_test), min_, max_ = load_cifar10()
-
-mean = np.mean(x_train,axis=(0,1,2,3))
-std = np.std(x_train,axis=(0,1,2,3))
-x_train = (x_train-mean)/(std+1e-7)
-x_test = (x_test-mean)/(std+1e-7)
-
-x_train = np.transpose(x_train, [0, 3,1,2])
-x_test = np.transpose(x_test, [0, 3,1,2])
-
-min_ = (min_-mean)/(std+1e-7)
-max_ = (max_-mean)/(std+1e-7)
-
-print (mean, std)
-
-# %%
 def data_processing(x_train, y_train, x_test, y_test, upsampler, device=device):
     
     if upsampler is None:
@@ -101,11 +53,6 @@ def data_processing(x_train, y_train, x_test, y_test, upsampler, device=device):
 
     return x_tensor, y_tensor, x_tensor_test, y_tensor_test
 
-
-# %%
-x_train, y_train, x_test, y_test = data_processing(x_train, y_train, x_test, y_test, upsampler=None)
-
-# %%
 def _testAccuracy(model, test_loader, transform=None, max_steps=10):
     model_was_training = model.training
     model.eval()
@@ -133,7 +80,6 @@ def _testAccuracy(model, test_loader, transform=None, max_steps=10):
     return(accuracy)
 
 
-# %%
 def train_model(model, optimizer, loss_fn, x_train, y_train, x_test, y_test, x_trigger=None, y_trigger=None, transform=None, batch_size=128, epochs=80):
     
     model.to(device)
@@ -187,19 +133,5 @@ def train_model(model, optimizer, loss_fn, x_train, y_train, x_test, y_test, x_t
 
 
 
-# %%
-model_checkpoint_path = './state_dicts/poison_%s_cifar10_model.pt' % MODEL_NAME
-if os.path.isfile(model_checkpoint_path):
-    checkpoint = torch.load(model_checkpoint_path, map_location=device)
-    model.load_state_dict(checkpoint['model_state_dict'])
-    optimizer.load_state_dict(checkpoint['optimizer_state_dict'])    
-    print('loaded model checkpoint')
-else:
-    model = train_model(model, optimizer, loss_fn, x_train, y_train, x_test, y_test, transform=None, epochs=80)
-    checkpoint = {'model_state_dict': model.state_dict(),'optimizer_state_dict' :optimizer.state_dict()}
-    torch.save(checkpoint, model_checkpoint_path)
-    print('saved model checkpoint')
-
-print("Model and data preparation done.")
 
 
